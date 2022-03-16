55c6e9ccc40faa49a852acb073786f7a651753c9
João Pedro de Alcântara Lima <joaopedro.ufma@gmail.com>
2022-03-16 11:00:07 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View , KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard, ScrollView} from 'react-native';
+import Task from './components/Task';
+
+import Icon from 'react-native-vector-icons/EvilIcons'
 
 export default function App() {
+  const [task,setTask] = useState('');
+  const [taskItems, setTaskItems] = useState([]);
+
+  function handleAddTask() {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  }
+
+  function completeTask(index) {
+    let itemsCopy = [...taskItems]
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.tasksWrapper}>
+      <Text style={styles.sectionTitle}>Tarefas do dia</Text>
+      
+      <View style={styles.items}>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      >
+        {
+          taskItems.map((item, index) => {
+            return (
+              <View style={styles.task} key={index} >
+                <Task title={item} />
+                <Icon name="trash" style={styles.trash} onPress={() => completeTask(index)} />
+              </View>
+              )
+          }
+            )
+        }
+      </ScrollView>
+      </View>
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput style={styles.input} placeholder={'Escreva a Tarefa'} value={task} onChangeText={text => setTask(text)} />
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -15,7 +68,56 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal:20
+  },
+  sectionTitle: {
+    fontSize:24,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  items: {
+    marginTop:30,
+    maxHeight: '75%',
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom:60,
+    width: '90%',
+    marginHorizontal: 20,
+    flexDirection:'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    padding:15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+    width: 250
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent:'center',
+    alignItems: 'center',
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+  },
+  addText: {
+
+  },
+  task: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  trash :{
+    fontSize:40
+  }
 });
---
+++ components/Task/index.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import { Text, View , StyleSheet} from 'react-native';
+
+export default function Task(props) {
+  return(
+    <View style={styles.item}>
+      <Text style={styles.itemText}>{props.title}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  item : {
+    marginVertical: 15
+  },
+  itemText: {
+    width: '100%',
+    fontSize: 20
+  },
+});
